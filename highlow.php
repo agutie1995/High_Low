1a4c706a1cce051a3f21c4e635aa5a1907d475b8
Alexandra Gutierrez <agutie95@yahoo.com>
2015-07-13 15:08:42 -0500

    added attempt counter

--- highlow.php
+++ highlow.php
@@ -2,7 +2,7 @@
 function playGame(){
 // game picks a random number between 1 and 100.
 $randomNumber = rand(1, 100);
-echo $randomNumber; 
+$attempts = 1;
 
 // prompts user to guess the number
 fwrite(STDOUT, 'Pick a number between 1 and 100: ');
@@ -11,10 +11,12 @@ $guess = trim(fgets(STDIN)) . PHP_EOL;
     do {
         // if user's guess is less than the number, it outputs "HIGHER"
         if ($guess < $randomNumber) {
+            $attempts++;
             fwrite(STDOUT, "HIGHER. Pick again. ") . PHP_EOL;
             $guess = trim(fgets(STDIN)) . PHP_EOL;
         } else if ($guess > $randomNumber) {
         // if user's guess is more than the number, it outputs "LOWER"
+            $attempts++;
             fwrite(STDOUT, "LOWER. Pick again. ") . PHP_EOL;
             $guess = trim(fgets(STDIN)) . PHP_EOL;
         }
@@ -23,6 +25,7 @@ $guess = trim(fgets(STDIN)) . PHP_EOL;
     // if a user guesses the number, the game should declare "GOOD GUESS!"
     if ($guess == $randomNumber){
         echo "YOU WIN! GOOD GUESS." . PHP_EOL;
+        echo "You guessed $attempts times.";
     }
 }
 playGame();
